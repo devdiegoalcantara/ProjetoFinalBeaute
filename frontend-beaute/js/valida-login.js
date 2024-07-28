e10b4555b31200ba3742824d588599b6849230ba
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-07-28 15:21:33 -0300

    Update valida-login.js

--- frontend-beaute/js/valida-login.js
+++ frontend-beaute/js/valida-login.js
@@ -1,17 +1,27 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.getElementById("login-form").addEventListener("submit", async function(event) {
+  event.preventDefault();
 
-    const email = event.target.elements["email"].value;
-    const password = event.target.elements["password"].value;
+  const email = event.target.elements["email"].value;
+  const senha = event.target.elements["password"].value;
 
-   
-    const dadosCadastro = JSON.parse(localStorage.getItem("cadastro"));
+  try {
+      const response = await fetch('http://localhost:3000/usuarios');
+      if (!response.ok) {
+          throw new Error('Erro ao buscar dados do usuário.');
+      }
 
-    if (dadosCadastro && dadosCadastro.email === email && dadosCadastro.password === password) {
-        const nome = dadosCadastro.nome.split(' ')[0];
-        alert(`Olá! Que bom que você voltou ${nome}.`);
-      window.location.href = "../index.html";
-    } else {
-      document.getElementById("login-message").textContent = "E-mail ou senha inválidos. Por favor, tente novamente.";
-    }
-  });
+      const usuarios = await response.json();
+
+      const usuario = usuarios.find(user => user.email === email && user.senha === senha);
+
+      if (usuario) {
+          const nome = usuario.nome.split(' ')[0];
+          alert(`Olá! Que bom que você voltou ${nome}.`);
+          window.location.href = "../index.html";
+      } else {
+          document.getElementById("login-message").textContent = "E-mail ou senha inválidos. Por favor, tente novamente.";
+      }
+  } catch (error) {
+      document.getElementById("login-message").textContent = "Erro ao tentar realizar login. Por favor, tente novamente mais tarde.";
+  }
+});
